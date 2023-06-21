050e502f07f177d9671e9b5400069fc1ac432528
furkanword <arcekevin898@gmail.com>
2023-06-21 09:15:47 -0500

    feat: :fire: C# arrays pruebas
    buscar materia

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+
+internal class NewBaseType{
+    private static void Main(string[] args)
+    {
+        string ? palabra;
+        ArrayList Materias = new ArrayList();
+        
+        Materias.Add("0) Calculo.");
+        Materias.Add("1) Español.");
+        Materias.Add("2) Dibujo tecnico.");
+        Materias.Add("3) Ingles.");
+
+        Console.Write("Escriba la materia a buscar: ");
+        palabra = Console.ReadLine();
+        Console.WriteLine(Materias.Contains(palabra) ? "La materia esta disponible":"La materia no se encontro");
+
+        do{
+            Console.Write("Ingrese la posicion donde desea insertar la asignatura: ");
+            idx = Convert.ToInt32(Console.ReadLine());
+        }while(idx > Materias.Count);
+
+        Materias.Insert(idx,palabra);
+
+        foreach(object materia in Materias){
+            Console.WriteLine(materia.ToString());
+        }
+    }
+ 
+}
